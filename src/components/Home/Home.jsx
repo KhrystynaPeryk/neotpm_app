8acf9b520c9cfb60c0b9b08fddb45d309cf55867
Khrystyna Peryk <kristik991@gmail.com>
2023-06-01 20:55:23 +0400

    Initial commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Home.scss'
 import Header from './components/Header'
 import Main from './components/Main'
-import PopUp from './components/PopUp'
+import WhatsAppIcon from '../../assets/images/whatsapp-icon.png';
 
 const Home = () => {
     const [isPopupOpen, setPopupOpen] = useState(true);
@@ -10,6 +10,10 @@ const Home = () => {
     const togglePopup = () => {
         setPopupOpen(!isPopupOpen);
     };
+
+    const handleRedirectToQuiz = () => {
+        window.open('https://transparentpm.ae/', '_blank');
+    }
   return (
     <div className='home-container'>
         <Header />
@@ -19,7 +23,19 @@ const Home = () => {
                 <span className='popup-button-text'>Quiz</span>
             </div>
         )}
-        <PopUp className={isPopupOpen ? 'popup-container open' : 'popup-container'} />
+        <div className={isPopupOpen ? 'popup-container open' : 'popup-container'}>
+            <div className='popup-close-icon' onClick={togglePopup}>&times;</div>
+            <div className='popup-text'>
+                <h1>Quiz Time:</h1>
+                <p>Uncover Your Personalized <span>Power of Attorney</span> Services in Just a Few Clicks!</p>
+                <button type='button' onClick={handleRedirectToQuiz}>START</button>
+            </div>
+        </div>
+        <a href='https://api.whatsapp.com/send?phone=971508486964' target='_blank' rel='noopener noreferrer'>
+            <div className='whatsapp-icon'>
+                <img src={WhatsAppIcon} alt='WhatsApp' />
+            </div>
+        </a>
     </div>
   )
 }
