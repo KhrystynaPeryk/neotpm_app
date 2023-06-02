30ff19726325ac780e9955de6ad795fd0f23e9df
Khrystyna Peryk <kristik991@gmail.com>
2023-06-02 09:48:04 +0400

    initial

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,30 +14,30 @@ const Home = () => {
     const handleRedirectToQuiz = () => {
         window.open('https://transparentpm.ae/', '_blank');
     }
-  return (
-    <div className='home-container'>
-        <Header />
-        <Main />
-        {!isPopupOpen && (
-            <div className='popup-button' onClick={togglePopup}>
-                <span className='popup-button-text'>Quiz</span>
+    return (
+        <div className='home-container'>
+            <Header />
+            <Main />
+            {!isPopupOpen && (
+                <div className='popup-button' onClick={togglePopup}>
+                    <span className='popup-button-text'>Quiz</span>
+                </div>
+            )}
+            <div className={isPopupOpen ? 'popup-container open' : 'popup-container'}>
+                <div className='popup-close-icon' onClick={togglePopup}>&times;</div>
+                <div className='popup-text'>
+                    <h1>Quiz Time:</h1>
+                    <p>Uncover Your Personalized <span>Power of Attorney</span> Services in Just a Few Clicks!</p>
+                    <button type='button' onClick={handleRedirectToQuiz}>START</button>
+                </div>
             </div>
-        )}
-        <div className={isPopupOpen ? 'popup-container open' : 'popup-container'}>
-            <div className='popup-close-icon' onClick={togglePopup}>&times;</div>
-            <div className='popup-text'>
-                <h1>Quiz Time:</h1>
-                <p>Uncover Your Personalized <span>Power of Attorney</span> Services in Just a Few Clicks!</p>
-                <button type='button' onClick={handleRedirectToQuiz}>START</button>
-            </div>
+            <a href='https://api.whatsapp.com/send?phone=971508486964' target='_blank' rel='noopener noreferrer'>
+                <div className='whatsapp-icon'>
+                    <img src={WhatsAppIcon} alt='WhatsApp' />
+                </div>
+            </a>
         </div>
-        <a href='https://api.whatsapp.com/send?phone=971508486964' target='_blank' rel='noopener noreferrer'>
-            <div className='whatsapp-icon'>
-                <img src={WhatsAppIcon} alt='WhatsApp' />
-            </div>
-        </a>
-    </div>
-  )
+    )
 }
 
 export default Home
