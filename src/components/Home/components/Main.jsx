780f0e691e85427815698115c4df52d47cce1323
Khrystyna Peryk <kristik991@gmail.com>
2023-06-02 12:37:59 +0400

    added btn to main

--- src/components/Home/components/Main.jsx
+++ src/components/Home/components/Main.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import './Main.scss'
 
 const Main = () => {
+
+  const handleRedirect = () => {
+    window.open('https://transparentpm.ae/', '_blank');
+  }
+
   return (
-    <div className='main-container'>
+
+      <div className='main-container'>
         <div className='main-text-box'>
             <p>You can stop looking for the perfect agents. You've found them!</p>
         </div>
-    </div>
+        <button className='main-btn' type='button' onClick={handleRedirect}>Discover our services</button>
+      </div>
+
   )
 }
 
