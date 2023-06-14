3ad802fb7913235df5f79fc91327d77aee17527b
Khrystyna Peryk <kristik991@gmail.com>
2023-06-14 21:35:05 +0400

    animation for img

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,14 +6,6 @@ import WhatsAppIcon from '../../assets/images/whatsapp-icon.png';
 
 const Home = () => {
     const [isPopupOpen, setPopupOpen] = useState(true);
-
-    const myFirstIngRef = useRef();
-    const mySecondIngRef = useRef();
-    const myThirdIngRef = useRef();
-    const [myFirstElementVisible, setMyFirstElementVisible] = useState(false);
-    const [mySecondElementVisible, setMySecondElementVisible] = useState(false);
-    const [myThirdElementVisible, setMyThirdElementVisible] = useState(false);
-
     const togglePopup = () => {
         setPopupOpen(!isPopupOpen);
     };
@@ -22,7 +14,14 @@ const Home = () => {
         window.open('https://transparent-poa.involve.me/poa-uae/', '_blank');
     };
 
-    //useEffect for handling of fade-in animation for .home-services-content
+    //below const and useEffect for handling of fade-in animation for .home-services-content
+    const myFirstIngRef = useRef();
+    const mySecondIngRef = useRef();
+    const myThirdIngRef = useRef();
+    const [myFirstElementVisible, setMyFirstElementVisible] = useState(false);
+    const [mySecondElementVisible, setMySecondElementVisible] = useState(false);
+    const [myThirdElementVisible, setMyThirdElementVisible] = useState(false);
+
     useEffect(() => {
         const handleIntersection = (entries) => {
             entries.forEach((entry) => {
@@ -34,46 +33,38 @@ const Home = () => {
                     } else if (entry.target === myThirdIngRef.current) {
                     setMyThirdElementVisible(true);
                     }
-                } else {
-                    if (entry.target === myFirstIngRef.current) {
-                    setMyFirstElementVisible(false);
-                    } else if (entry.target === mySecondIngRef.current) {
-                    setMySecondElementVisible(false);
-                    } else if (entry.target === myThirdIngRef.current) {
-                    setMyThirdElementVisible(false);
-                    }
                 }
             });
         };
     
         const options = {
-          threshold: 0.2,
+            threshold: 0.2,
         };
     
         const observer = new IntersectionObserver(handleIntersection, options);
     
         if (myFirstIngRef.current) {
-          observer.observe(myFirstIngRef.current);
+            observer.observe(myFirstIngRef.current);
         }
     
         if (mySecondIngRef.current) {
-          observer.observe(mySecondIngRef.current);
+            observer.observe(mySecondIngRef.current);
         }
     
         if (myThirdIngRef.current) {
-          observer.observe(myThirdIngRef.current);
+            observer.observe(myThirdIngRef.current);
         }
     
         return () => {
-          if (myFirstIngRef.current) {
-            observer.unobserve(myFirstIngRef.current);
-          }
-          if (mySecondIngRef.current) {
-            observer.unobserve(mySecondIngRef.current);
-          }
-          if (myThirdIngRef.current) {
-            observer.unobserve(myThirdIngRef.current);
-          }
+            if (myFirstIngRef.current) {
+                observer.unobserve(myFirstIngRef.current);
+            }
+            if (mySecondIngRef.current) {
+                observer.unobserve(mySecondIngRef.current);
+            }
+            if (myThirdIngRef.current) {
+                observer.unobserve(myThirdIngRef.current);
+            }
         };
     }, []);
 
@@ -112,36 +103,36 @@ const Home = () => {
             className={`home-services-content ${myFirstElementVisible ? 'fade-in' : ''}`}
             ref={myFirstIngRef}
             >
-            <h1>Property Management</h1>
-            <p>Effortless Property Management Solutions: Leave the Hassle to Us</p>
-            <div className="btns-container">
-                <button type="button">Buy</button>
-                <button type="button">Learn More</button>
-            </div>
+                <h1>Property Management</h1>
+                <p>Effortless Property Management Solutions: Leave the Hassle to Us</p>
+                <div className="btns-container">
+                    <button type="button">Buy</button>
+                    <button type="button">Learn More</button>
+                </div>
             </div>
             <div className="home-services-image first-service-img" />
             <div
             className={`home-services-content ${mySecondElementVisible ? 'fade-in' : ''}`}
             ref={mySecondIngRef}
             >
-            <h1>Property Maintenance</h1>
-            <p>Maintain and Enhance Your Property: Expert Property Maintenance Services for Lasting Value</p>
-            <div className="btns-container">
-                <button type="button">Buy</button>
-                <button type="button">Learn More</button>
-            </div>
+                <h1>Property Maintenance</h1>
+                <p>Maintain and Enhance Your Property: Expert Property Maintenance Services for Lasting Value</p>
+                <div className="btns-container">
+                    <button type="button">Buy</button>
+                    <button type="button">Learn More</button>
+                </div>
             </div>
             <div className="home-services-image second-service-img" />
             <div
             className={`home-services-content ${myThirdElementVisible ? 'fade-in' : ''}`}
             ref={myThirdIngRef}
             >
-            <h1>Legal Documentation Support</h1>
-            <p>Simplify Property Processes with Expert Legal Documentation Support</p>
-            <div className="btns-container">
-                <button type="button">Buy</button>
-                <button type="button">Learn More</button>
-            </div>
+                <h1>Legal Documentation Support</h1>
+                <p>Simplify Property Processes with Expert Legal Documentation Support</p>
+                <div className="btns-container">
+                    <button type="button">Buy</button>
+                    <button type="button">Learn More</button>
+                </div>
             </div>
             <div className="home-services-image third-service-img" />
         </section>
