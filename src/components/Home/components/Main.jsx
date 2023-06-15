5d6db686cb3c1ad4c530fbf67cc15b22a73a99e2
Khrystyna Peryk <kristik991@gmail.com>
2023-06-15 13:47:52 +0400

    icons services

--- src/components/Home/components/Main.jsx
+++ src/components/Home/components/Main.jsx
@@ -8,14 +8,12 @@ const Main = () => {
   }
 
   return (
-
       <div className='main-container'>
         <div className='main-text-box'>
             <p>You can stop looking for the perfect agents. You've found them!</p>
         </div>
         <button className='main-btn' type='button' onClick={handleRedirect}>Discover our services</button>
       </div>
-
   )
 }
 
--- src/components/PropertyManagement/PropertyManagement.jsx
+++ src/components/PropertyManagement/PropertyManagement.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './PropertyManagement.scss'
+import NavBar from '../common/NavBar/NavBar'
+import Logo from '../common/Logo/Logo'
+import LightHouse from '../../assets/images/light_house.png'
+import StandardHouse from '../../assets/images/standard_house.png'
+import ExecutiveHouse from '../../assets/images/executive_house.png'
 
 const PropertyManagement = () => {
   return (
-    <div>PropertyManagement</div>
+    <div className='property-container'>
+        <div>
+            <NavBar />
+            <div className='header-container'>
+                <div className='logo-container'>
+                    <Logo />
+                </div>
+            </div>
+            <div className='main-container'>
+                <div className='main-text-box property-header-container'>
+                    <p className='header'>Our Property Management Packages</p>
+                </div>
+            </div>
+        </div>
+        <div className='property-table-wrapper'>
+            <section>
+                <div>
+                    <img className='property-icon' src={LightHouse} alt='LightHouse' />
+                </div>
+                <div>LIGHT</div>
+            </section>
+            <section>
+                <div>
+                    <img className='property-icon' src={StandardHouse} alt='StandardHouse' />
+                </div>
+                <div>STANDARD</div>
+            </section>
+            <section>
+                <div>
+                    <img className='property-icon' src={ExecutiveHouse} alt='ExecutiveHouse' />
+                </div>
+                <div>EXECUTIVE</div>
+            </section>
+        </div>
+    </div>
   )
 }
 
