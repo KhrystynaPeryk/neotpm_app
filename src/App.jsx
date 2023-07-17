868d147b7cfb84fd169387dd72e81569ce4db3f8
Khrystyna Peryk <kristik991@gmail.com>
2023-07-17 10:42:18 +0400

    pop-up setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import React, { useState, useEffect } from 'react';
 import Home from './components/Home/Home';
 import PropertyManagement from './components/PropertyManagement/PropertyManagement';
 import PropertyMaintenance from './components/PropertyMaintenance/PropertyMaintenance';
@@ -7,10 +8,23 @@ import DocumentsRequestForm from './components/DocumentsRequestForm/DocumentsReq
 import PowerOfAttorney from './components/OwnerSupport/components/PowerOfAttorney/PowerOfAttorney';
 import Cart from './components/Cart/Cart';
 import { Route, Routes } from 'react-router-dom';
+import LeadGenerationPopUp from './components/common/LeadGenerationPopUp/LeadGenerationPopUp';
 
 function App() {
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
+    <div className={showPopup && 'overlay-popup'}>
+      {showPopup && <LeadGenerationPopUp />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/property-management' element={<PropertyManagement />} />
---
+++ src/components/common/LeadGenerationPopUp/LeadGenerationPopUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './LeadGenerationPopUp.scss'
+
+const LeadGenerationPopUp = () => {
+  return (
+    <div className='lead-popup-container'>LeadGenerationPopUp</div>
+  )
+}
+
+export default LeadGenerationPopUp
